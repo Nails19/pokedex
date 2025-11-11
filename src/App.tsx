1b0397a4fe7ccaf25e462629e44de112782cd64a
Nails8919 <shorty_love_12@hotmail.com>
2025-11-11 15:51:45 -0400

    Further additions to the code

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
-import { randomInt } from "crypto"
 import { useEffect, useState } from "react"
 
 function App() {
 
-const [pokemon, setPokemon] = useState(null)
+const [pokemon, setPokemon] = useState<any>(null)
 
 useEffect(() => {
+const randomInt = (min:number, max:number) => {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
   const URL:string="https://pokeapi.co/api/v2/pokemon/"+randomInt(1,152)
   fetch(URL)
   .then(Response => Response.json())
@@ -16,7 +19,11 @@ useEffect(() => {
   return (
     <>
     <p className= "text-4xl">{pokemon?.name}</p>
-    <p className="text-4xl">{pokemon?.abilities?.[0]?.ability?.name}</p>
+    {
+      pokemon?.abilities.map(skill => {
+        return <p className="text-2xl" key={skill.ability.name}>{skill.ability.name}</p>
+      })
+    }
     </>
   )
 }
